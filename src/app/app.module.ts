adbe33e78a7f91e5ea00806117226b16126c5186
jpmitri <60190789+jpmitri@users.noreply.github.com>
2020-11-12 03:49:40 +0200

    angular creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './content/about/about.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { ServicesComponent } from './content/services/services.component';
+import { TeamComponent } from './content/team/team.component';
+import { HomeComponent } from './content/home/home.component';
+
+
+import { RouterModule, Routes } from '@angular/router';
+
+const JusticiAllRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'team', component: TeamComponent },
+  { path: '', component: HomeComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    TeamComponent,
+    HomeComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(JusticiAllRoutes)
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
